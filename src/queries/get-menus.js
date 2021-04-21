01b4048b97bc1656666e46374f1f8733f7915d1b
dantursunov <35630476+dantursunov@users.noreply.github.com>
2021-04-22 04:30:45 +0600

    Add files via upload

---
+++ src/queries/get-menus.js
@@ -0,0 +1,34 @@
+import { gql } from "@apollo/client";
+import MenuFragment from "./fragments/menus";
+
+export const GET_MENUS = gql`
+  headerMenus: menuItems(where: {location: HCMS_MENU_HEADER}) {
+    edges {
+      node {
+        ...MenuFragment
+        childItems {
+          edges {
+            node {
+            ...MenuFragment
+            }
+          }
+        }
+      }
+    }
+  }
+  footerMenus: menuItems(where: {location: HCMS_MENU_FOOTER}) {
+    edges {
+      node {
+        ...MenuFragment
+        childItems {
+          edges {
+            node {
+            ...MenuFragment
+            }
+          }
+        }
+      }
+    }
+  }
+  ${MenuFragment}
+`
