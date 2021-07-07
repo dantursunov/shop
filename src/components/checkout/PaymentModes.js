8858e19cd6518274ffbe02941e3721ab87319e59
jawadan <35630476+jawadan@users.noreply.github.com>
2021-07-07 18:57:44 +0600

    Update PaymentModes.js

--- src/components/checkout/PaymentModes.js
+++ src/components/checkout/PaymentModes.js
@@ -8,12 +8,12 @@ const PaymentModes = ( { input, handleOnChange } ) => {
 		<div className="mt-3">
 			<Error errors={ errors } fieldName={ 'paymentMethod' }/>
 			{/*Direct bank transfers*/}
-			<div className="form-check woo-next-payment-input-container mt-2">
+			{/*<div className="form-check woo-next-payment-input-container mt-2">
 				<label className="form-check-label">
 					<input onChange={ handleOnChange } value="bacs" className="form-check-input mr-3" name="paymentMethod" type="radio" checked={'bacs' === paymentMethod}/>
 					<span className="woo-next-payment-content">Direct Bank Transfer</span>
 				</label>
-			</div>
+			</div>*/}
 			{/*Pay with Paypal*/}
 			<div className="form-check woo-next-payment-input-container mt-2">
 				<label className="form-check-label">
@@ -21,14 +21,14 @@ const PaymentModes = ( { input, handleOnChange } ) => {
 					<span className="woo-next-payment-content">Pay with Paypal</span>
 				</label>
 			</div>
-			{/*Check Payments*/}
-			<div className="form-check woo-next-payment-input-container mt-2">
+			
+			{/*<div className="form-check woo-next-payment-input-container mt-2">
 				<label className="form-check-label">
 					<input onChange={ handleOnChange } value="cheque" className="form-check-input mr-3" name="paymentMethod" type="radio" checked={'cheque' === paymentMethod}/>
 					<span className="woo-next-payment-content">Check Payments</span>
 				</label>
 			</div>
-			{/*Pay with Stripe*/}
+			
 			<div className="form-check woo-next-payment-input-container mt-2">
 				<label className="form-check-label">
 					<input onChange={ handleOnChange } value="cod" className="form-check-input mr-3" name="paymentMethod" type="radio" checked={'cod' === paymentMethod}/>
@@ -47,10 +47,10 @@ const PaymentModes = ( { input, handleOnChange } ) => {
 					<span className="woo-next-payment-content">CC Avenue</span>
 				</label>
 			</div>
-			{/*	Payment Instructions*/}
+			
 			<div className="woo-next-checkout-payment-instructions mt-2">
 				Please send a check to Store Name, Store Street, Store Town, Store State / County, Store Postcode.
-			</div>
+			</div>*/}
 		</div>
 	);
 };
