5df69ddd4ae2ff66d7f5f56e36869e06e99c8429
jawadan <35630476+jawadan@users.noreply.github.com>
2021-07-20 07:52:18 +0600

    latest

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -1,21 +1,63 @@
-import {Facebook, Instagram, Twitter, Youtube} from "../../icons";
+import {isEmpty, isArray} from 'lodash';
+import {sanitize} from '../../../utils/miscellaneous';
+import Link from 'next/link';
+import {getIconComponentByName} from '../../../utils/icons-map';
+import NewsletterSubscribe from './NewsletterSubscribe';
 
-const Footer = () => (
-	<div className="footer bg-black p-6 text-red-300">
-		<div className="container mx-auto">
-			<div className="footer-text flex-none md:flex items-center justify-between">
-				<p>© Onzombie 2021</p>
-				<p className="text-gray">Be prepared for any situation</p>
-				<span className="text-gray">Follow on social links to support the work</span>
+const Footer = ( {footer, footerMenus} ) => {
+	return (
+		<footer className="bg-teal-500 p-6">
+			<div className="flex flex-wrap -mx-1 overflow-hidden text-white">
+
+				{/*Widget One*/}
+				<div className="my-1 px-1 w-full overflow-hidden sm:w-full lg:w-1/2 xl:w-1/3">
+					<div dangerouslySetInnerHTML={{ __html: sanitize( footer?.sidebarOne ) }}/>
+				</div>
+
+				{/*Widget Two*/}
+				<div className="my-1 px-1 w-full overflow-hidden sm:w-full lg:w-1/2 xl:w-1/3">
+					<div dangerouslySetInnerHTML={{ __html: sanitize( footer?.sidebarTwo ) }}/>
+				</div>
+
+				<div className="my-1 px-1 w-full overflow-hidden sm:w-full lg:w-1/2 xl:w-1/3">
+          {/*Mailchimp Newsletter Subscription*/}
+          <NewsletterSubscribe/>
+          {/* Footer Menus*/}
+					{ ! isEmpty( footerMenus ) && isArray( footerMenus ) ? (
+						<ul>
+							{ footerMenus.map( footerMenu => (
+								<li key={footerMenu?.node?.id}>
+									<Link href={footerMenu?.node?.path}>
+										<a>
+											{footerMenu?.node?.label}
+										</a>
+									</Link>
+								</li>
+							) ) }
+						</ul>
+					) : null }
+
+				</div>
 			</div>
-			<ul className="social-links mt-8 flex align-center">
-				<li><a href="https://www.facebook.com" className="fa fa-facebook" target="_blank"><Facebook/></a></li>
-				<li className="ml-2 mt-1"><a href="https://twitter.com" target="_blank"><Twitter/></a></li>
-				<li className="ml-2 mt-1"><a href="https://youtube.com" className="fa fa-youtube" target="_blank"><Youtube/></a></li>
-				<li className="ml-2"><a href="https://www.instagram.com" className="fa fa-instagram" target="_blank"><Instagram/></a></li>
-			</ul>
-		</div>
-	</div>
-);
+			{/*Copyright Text*/}
+			<div className="mb-8 mt-8 w-full flex flex-wrap">
+				<div className="w-full md:w-1/2 lg:w-1/4 text-white">{footer?.copyrightText ? footer.copyrightText : '© Codeytek Academy 2020'}</div>
+				<div className="w-full lg:w-3/4 flex justify-end">
+					{ ! isEmpty( footer?.socialLinks ) && isArray( footer?.socialLinks ) ? (
+						<ul className="flex items-center">
+							{ footer.socialLinks.map( socialLink => (
+								<li key={ socialLink?.iconName } className="ml-4">
+									<a href={socialLink?.iconUrl}>
+										{ getIconComponentByName( socialLink?.iconName ) }
+									</a>
+								</li>
+							) ) }
+						</ul>
+					) : null }
+				</div>
+			</div>
+		</footer>
+	);
+};
 
 export default Footer;
